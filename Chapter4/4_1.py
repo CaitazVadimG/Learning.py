80d35f84eaed7d8daf12a84df20804e320418561
CaitazVadimG <vadimcaitaz09@gmail.com>
2026-05-05 00:14:15 +0300

    Chapter4 Third Add

--- Chapter4/4_1.py
+++ Chapter4/4_1.py
@@ -4,5 +4,11 @@ print(math.sqrt(85))
 import random
 print(random.random())
 print(random.choice([1,2,3,4]))
+S='Spam'
+print(len(S))
+print(S[0])
+print(S[1])
+print(S[-1])
+print(S[-2])
 #3.1415*2
 #print(3.1415*2)
