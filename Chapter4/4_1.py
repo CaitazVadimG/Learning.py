8b90606962cef8273f26dbebecae7f50d69577e2
CaitazVadimG <vadimcaitaz09@gmail.com>
2026-05-05 12:32:40 +0300

    Chapter4 4th Add

--- Chapter4/4_1.py
+++ Chapter4/4_1.py
@@ -1,14 +1,21 @@
 import math
-print(math.pi)
-print(math.sqrt(85))
+#print(math.pi)
+#print(math.sqrt(85))
 import random
-print(random.random())
-print(random.choice([1,2,3,4]))
+#print(random.random())
+#print(random.choice([1,2,3,4]))
 S='Spam'
-print(len(S))
-print(S[0])
-print(S[1])
-print(S[-1])
-print(S[-2])
-#3.1415*2
-#print(3.1415*2)
+#print(len(S))
+#print(S[0])
+#print(S[1])
+#print(S[-1])
+#print(S[-2])
+#print(S)
+#print(S[1:3])
+#print(S[0:4])
+#print(S+'xyz')
+#S='z' + S
+print(S)
+print(S.find('pa'))
+print(S.replace('pa','XYZ'))
+print(S)
