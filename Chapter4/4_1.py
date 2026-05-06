bc50cf4a187668afbf69e71a6d1da28e4260bea4
CaitazVadimG <vadimcaitaz09@gmail.com>
2026-05-06 12:38:10 +0300

    Chapter4 For Loop

--- Chapter4/4_1.py
+++ Chapter4/4_1.py
@@ -1,10 +1,10 @@
-import math
+#import math
 #print(math.pi)
 #print(math.sqrt(85))
-import random
+#import random
 #print(random.random())
 #print(random.choice([1,2,3,4]))
-S='Spam'
+#S='Spam'
 #print(len(S))
 #print(S[0])
 #print(S[1])
@@ -15,7 +15,19 @@ S='Spam'
 #print(S[0:4])
 #print(S+'xyz')
 #S='z' + S
-print(S)
-print(S.find('pa'))
-print(S.replace('pa','XYZ'))
-print(S)
+#print(S)
+#print(S.find('pa'))
+#print(S.replace('pa','XYZ'))
+#print(S)
+#line = 'aaa,bbb,ccc,ddd\n'
+#print(line)
+#line = line.rstrip()
+#print(line.split(','))
+#print(S.upper())
+#print(S.isalpha())
+#print('%s, eggs, and %s' % ('spam', 'SPAM!'))
+#print('{0}, eggs, and {1}'.format('spam', 'SPAM!'))
+squares = []
+for i in range(10,101):
+    squares.append(i ** 2)
+print(squares)
