fbc5e96de68affabecbc0df8927cb9590187d64b
CaitazVadimG <vadimcaitaz09@gmail.com>
2026-05-09 15:04:09 +0300

    Chapter 5 Test 2

--- Chapter5/1.py
+++ Chapter5/1.py
@@ -1,8 +1,19 @@
 x=5
 y=1
-print(x + y)
-print(x - y)
-print(x * y)
-print(x ** y)
-print(x % y)
-print(x & y)
+z=4
+w=10
+print('x = ',x)
+print('y = ',y)
+print('z = ',z)
+print('w = ',w)
+print('x + y = ',x + y,'.')
+print('x - y = ',x - y,'.')
+print('x * y = ',x * y,'.')
+print('x ** y = ',x ** y,'.')
+print('x % y = ',x % y,'.')
+print('x & y = ',x & y,'.')
+print('y==1' if y==1 else 'y!=1')
+print('type(y)=',type(y),'.')
+print('type(x)=',type(x),'.')
+print('x and y have the same type .' if type(x)==type(y) else 'x and y does not have the same type .')
+print('x * y + z * w =',x * y + z * w)
