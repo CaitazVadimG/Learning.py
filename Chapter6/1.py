7661d6416e87ca1a783325e0697e397dcbf9c135
CaitazVadimG <vadimcaitaz09@gmail.com>
2026-05-25 17:03:00 +0300

    Dynamic Typisation 2

--- Chapter6/1.py
+++ Chapter6/1.py
@@ -7,3 +7,17 @@ print("type(a) = ", type(a) )
 a = 1.23
 print("a = ", a)
 print("type(a) = ", type(a) )
+
+a = 3
+b = a
+a = 'spam'
+
+print("a = ", a)
+print("b = ", b)
+
+a = 3
+b = a
+a = a + 2
+
+print("a = ", a)
+print("b = ", b)
