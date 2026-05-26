f769d12fb048d9b0b2ec7cdcd89a266d8d43c8a7
CaitazVadimG <vadimcaitaz09@gmail.com>
2026-05-26 17:03:00 +0300

    Dynamic Typisation 3

--- Chapter6/1.py
+++ Chapter6/1.py
@@ -21,3 +21,17 @@ a = a + 2
 
 print("a = ", a)
 print("b = ", b)
+
+L1 = [2, 3, 4]
+L2 = L1
+L1[0] = 24
+
+print("L1 = ", L1)
+print("L2 = ", L2)
+
+L1 = [2, 3, 4]
+L2 = L1[:]
+L1[0] = 24
+
+print("L1 = ", L1)
+print("L2 = ", L2)
