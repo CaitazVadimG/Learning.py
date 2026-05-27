546fb786aa003298a99e7413ae25afcb12e19cfc
CaitazVadimG <vadimcaitaz09@gmail.com>
2026-05-27 17:03:00 +0300

    Dynamic Typisation 4

--- Chapter6/1.py
+++ Chapter6/1.py
@@ -35,3 +35,17 @@ L1[0] = 24
 
 print("L1 = ", L1)
 print("L2 = ", L2)
+
+L = [1, 2, 3]
+M = L
+print("L = ", L)
+print("M = ", M)
+print("(L == M) = ", L == M)
+print("(L is M) = ", L is M)
+
+L = [1, 2, 3]
+M = [1, 2, 3]
+print("L = ", L)
+print("M = ", M)
+print("(L == M) = ", L == M)
+print("(L is M) = ", L is M)
