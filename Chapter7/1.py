9ac0b773cc891947e98fedee9d2b7d29b3b41676
CaitazVadimG <vadimcaitaz09@gmail.com>
2026-05-30 17:03:00 +0300

    String formats

--- Chapter7/1.py
+++ Chapter7/1.py
@@ -3,3 +3,9 @@ print(" 'abc' + 'def'  = ",'abc' + 'def')
 print(" 'Ni!' * 4 = ", 'Ni!' * 4)
 
 print('-' * 80)
+
+
+S = 'spam'
+print("S[0], S[−2] = ",S[0], S[-2])
+print("S[1:3], S[1:], S[:−1] = ",S[1:3], S[1:], S[:-1])
+print('That is %d %s bird!' % (1, 'dead'))
