fcac3af68b0a031186f351a460a55e42b98ef961
CaitazVadimG <vadimcaitaz09@gmail.com>
2026-05-31 17:03:00 +0300

    String formats final

--- Chapter7/1.py
+++ Chapter7/1.py
@@ -9,3 +9,8 @@ S = 'spam'
 print("S[0], S[−2] = ",S[0], S[-2])
 print("S[1:3], S[1:], S[:−1] = ",S[1:3], S[1:], S[:-1])
 print('That is %d %s bird!' % (1, 'dead'))
+print('That is {0} {1} bird!'.format(1, 'dead'))
+S = "s,pa,m"
+print('S = {0}'.format(S))
+print('S[2:4] = {0}'.format(S[2:4]))
+print("S.split(',')[1] = {0}".format(S.split(',')[1]))
